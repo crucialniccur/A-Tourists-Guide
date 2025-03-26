68291d3dc42331631912a3e2693ac445ad12f33b
Mati Joseph <crucialniccur@gmail.com>
2025-03-26 19:34:44 +0300

    access the countryInput field and set it equal to a variable

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-fetch("https://a-tourists-guide.onrender.com/api/users")
-  .then((res) => res.json())
-  .then(console.log);
+// fetch("https://a-tourists-guide.onrender.com/api/users")
+//   .then((res) => res.json())
+//   .then(console.log);
+
+//declare variables at the global context here
+let countyInput = document.querySelector("#country-code");
